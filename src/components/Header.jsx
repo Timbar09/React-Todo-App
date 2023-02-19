eab5f34c94c0f4e55b0ee3fce2bd113989955d72
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-19 07:43:56 +0200

    Add Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import Navbar from '@/components/Navbar';
+
 const Header = () => {
   return (
     <header>
       <h1>todos</h1>
       <p>Items will persist in the browser local storage</p>
+      <Navbar />
     </header>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { useState, useRef } from 'react';
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const ref = useRef();
+  console.log(ref);
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button onClick={() => setDropdown((prev) => !prev)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
