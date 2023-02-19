f51ac93d996a34a330ba48beb3c2f749c7cb6244
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-19 09:22:52 +0200

    Add last name, message and car brand inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,17 +2,56 @@ import './Form.css';
 import { useState } from 'react';
 
 const Form = () => {
-  const [fname, setFname] = useState('');
+  const [state, setState] = useState({
+    fname: '',
+    lname: '',
+    message: '',
+    carBrand: '',
+  });
+
+  const handleChange = (e) => {
+    setState((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const carBrands = ['Mercedes', 'BMW', 'Maserati', 'Infinity', 'Audi'];
+  const carBrandOptions = carBrands.map((carBrand, key) => (
+    <option value={carBrand} key={key}>
+      {carBrand}
+    </option>
+  ));
 
   return (
     <>
       <h1>Controlled Form</h1>
       <form>
         <label>
-          First Name: <input type="text" value={fname} />
+          First Name: <input type="text" value={state.fname} onChange={handleChange} name="fname" />
+        </label>
+        <label>
+          Last Name: <input type="text" value={state.lname} onChange={handleChange} name="lname" />
+        </label>
+        <label>
+          Message:
+          <textarea type="text" value={state.message} onChange={handleChange} name="message" />
+        </label>
+        <label>
+          Car Brand:
+          <select type="text" value={state.message} onChange={handleChange} name="message">
+            <option value={''} disabled>
+              --Pick a car brand--
+            </option>
+            {carBrandOptions}
+          </select>
         </label>
       </form>
-      <h5>First name: {fname}</h5>
+      <h5>
+        My name is {state.fname} {state.lname}
+      </h5>
+      <p>I would like to say, {state.message}</p>
+      <h5>Favorite car brand: {state.carBrand}</h5>
     </>
   );
 };
